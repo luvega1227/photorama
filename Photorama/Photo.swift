f19cdf0695f3514c8081571461525ec204f21456
Luis Vega <luisvega@Luiss-MacBook-Pro.local>
2016-05-27 14:40:49 -0700

    initial commit

---
+++ Photorama/Photo.swift
@@ -0,0 +1,31 @@
+
+
+import UIKit
+import CoreData
+
+class Photo: NSManagedObject {
+
+    var image: UIImage?
+    
+    override func awakeFromInsert() {
+        super.awakeFromInsert()
+        
+        // Give the properties their initial values
+        title = ""
+        photoID = ""
+        remoteURL = NSURL()
+        photoKey = NSUUID().UUIDString
+        dateTaken = NSDate()
+    }
+    
+    func addTagObject(tag: NSManagedObject) {
+        let currentTags = mutableSetValueForKey("tags")
+        currentTags.addObject(tag)
+    }
+    
+    func removeTagObject(tag: NSManagedObject) {
+        let currentTags = mutableSetValueForKey("tags")
+        currentTags.removeObject(tag)
+    }
+
+}
